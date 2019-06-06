42267692fdd754d4cd13a8815621318a1a17b919
Hugo Cabel <hugocabel4@gmail.com>
2019-06-06 14:35:16 +0200

    Oups pas a la norm le diplay.c

--- srcs/display/display.c
+++ srcs/display/display.c
@@ -6,26 +6,15 @@
 /*   By: hcabel <hcabel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 13:46:47 by hcabel            #+#    #+#             */
-/*   Updated: 2019/06/06 14:22:39 by hcabel           ###   ########.fr       */
+/*   Updated: 2019/06/06 14:34:47 by hcabel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-/*
-void	init_tab(void (**f[8])(void*))
-{
-	
-}*/
 
 void	display(t_flags *lst_flags, t_variable *lst_variable
 			, t_constchar *lst_constchar)
 {
-	void	(*f[4])(void*);
-	t_variable	*lst;
-
-	lst = lst_variable;
-	f[0] = put_cast_char;
-	f[1] = put_cast_str;
 	while (lst_flags || lst_variable || lst_constchar)
 	{
 		if (lst_constchar)
@@ -36,24 +25,17 @@ void	display(t_flags *lst_flags, t_variable *lst_variable
 		if (lst_variable && lst_flags)
 		{
 			if (lst_flags->type == 'c')
-				f[0](lst_variable->content);
+				ft_putstr("{char}");
 			else if (lst_flags->type == 's')
-				f[1](lst_variable->content);
+				ft_putstr("{string}");
 			else if (lst_flags->type == 'd')
 				ft_putstr("{number}");
 			else if (lst_flags->type == 'u')
 				ft_putstr("{unsigned number}");
 			lst_variable = lst_variable->next;
-			lst_flags =	lst_flags->next;
+			lst_flags = lst_flags->next;
 		}
 		else if (lst_flags && !lst_variable)
 			ft_putstr("{More variables please}");
 	}
-	while (lst)
-	{
-	/*	put_cast_signed(lst->content);
-		ft_putstr("\n");*/
-		lst = lst->next;
-	}
-	
 }
