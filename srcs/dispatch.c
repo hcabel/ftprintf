e72916d7a662850137fd13301e3180f593a4b30b
hcabel <hcabel@student.42.fr>
2019-07-09 15:59:41 +0200

    add space systeme

--- srcs/dispatch.c
+++ srcs/dispatch.c
@@ -6,69 +6,41 @@
 /*   By: hcabel <hcabel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/09 00:04:11 by hcabel            #+#    #+#             */
-/*   Updated: 2019/07/09 12:31:07 by hcabel           ###   ########.fr       */
+/*   Updated: 2019/07/09 15:57:31 by hcabel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-static void	setup_func(t_func *func_adr[7])
-{
-	t_func *func;
-
-	func = *func_adr;
-	//func[0].f = show_c;
-	func[0].type = 'c';
-	//func[1].f = show_s;
-	func[1].type = 's';
-	//func[2].f = show_p;
-	func[2].type = 'p';
-	//func[3].f = show_d;
-	func[3].type = 'd';
-	//func[4].f = show_o;
-	func[4].type = 'o';
-	//func[5].f = show_x;
-	func[5].type = 'x';
-	//func[6].f = show_u;
-	func[6].type = 'u';
-}
 
 static int	cal_size(void *arg, char type)
 {
 	if (type == 'c')
 		return (1);
 	else if (type == 's')
-		return (ft_strlen((char*)arg));
+		return ((char*)arg == NULL ? -1 : ft_strlen((char*)arg));
 	else if (type == 'p')
 		return (1);
 	else if (type == 'd' || type == 'i')
-		return (ft_nbrlen(arg));
+		return ((int)arg == NULL ? -1 : ft_nbrlen(arg));
 	else if (type == 'o')
 		return (1);
 	else if (type == 'x' || type == 'X')
-		return (ft_baselen(arg, 16));
+		return ((int)arg == NULL ? -1 : ft_baselen(arg, 16));
 	else if (type == 'u')
-		return (ft_nbrlen((char*)arg));
+		return ((int)arg == NULL ? -1 : ft_nbrlen(arg));
 }
 
 static char *convert_to_char(void *arg, char type, int size)
 {
-	int		i;
 	char	*str;
-	char	*string_arg;
 
-	if (type != 'x' || type != 'X')
+	if (type != 'x' || type != 'X' || type != 's')
 		if (!(str = (char*)malloc(sizeof(char) * size)))
 			return (NULL);
 	if (type == 'c')
 		str[0] = (char)arg;
 	else if (type == 's')
-	{
-		i = 0;
-		string_arg = (char*)arg;
-		while (string_arg[i])
-			str[i - 1] = string_arg[i++];
-	}
+		str = (char*)arg;
 	else if (type == 'p')
 		str[0] = type;
 	else if (type == 'd' || type == 'i')
@@ -79,17 +51,39 @@ static char *convert_to_char(void *arg, char type, int size)
 		str = ft_itoa_base(arg, 16);
 	else if (type == 'u')
 		str[0] = type;
-	return (str);
+	return (str != NULL ? str : NULL);
+}
+
+static void	fill(int size, char c)
+{
+	char	*str;
+	int		i;
+
+	if (size < 0 || (str = (char*)malloc(sizeof(char) * size)) == NULL)
+		return ;
+	i = 0;
+	while (i < size)
+		str[i++] = c;
+	write(1, str, size);
+	free(str);
 }
 
 void		pf_dispatch(t_flags flags, void *arg)
 {
-	t_func	func[7];
 	int		size;
 	char	*str_arg;
 
-	//setup_func(&func);
-	size = cal_size(arg, flags.type);
-	str_arg = convert_to_char(arg, flags.type, size);
-	ft_putstr(str_arg);
+	if ((size = cal_size(arg, flags.type)) != -1)
+		str_arg = convert_to_char(arg, flags.type, size);
+	else
+	{
+		str_arg = "(null)";
+		size = 6;
+	}
+	size = (size > flags.precis && flags.precis != -1 ? flags.precis : size);
+	if (flags.options[0] != '-')
+		fill(flags.length - size, ' ');
+	write(1, str_arg, size);
+	if (flags.options[0] == '-')
+		fill(flags.length - size, ' ');
 }
