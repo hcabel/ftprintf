3e553cfbb5b7ad19572a08d5aef3609e4d0922a7
Sylvia Lewis <sylewis@42.fr>
2019-10-06 18:06:28 +0200

    tu fais chier

--- srcs/ft_ftoa.c
+++ srcs/ft_ftoa.c
@@ -3,21 +3,76 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ftoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hcabel <hcabel@student.42.fr>              +#+  +:+       +#+        */
+/*   By: sylewis <sylewis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/04 19:30:09 by hcabel            #+#    #+#             */
-/*   Updated: 2019/09/04 20:42:12 by hcabel           ###   ########.fr       */
+/*   Updated: 2019/10/06 18:00:36 by sylewis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdlib.h>
+/*
+static void		rounding_realloc(char **str_addr, int sign)
+{
+	char	*str;
+	char	*new_str;
+	int		i;
+	int		j;
+
+	str = *str_addr;
+	j = 0;
+	if (!(new_str = (char*)ft_memalloc(sizeof(*new_str) * (ft_strlen(str) + 2))))
+		return ;
+	if (sign == -1)
+	{
+		new_str[1] = '1';
+		new_str[0] = '-';
+		i = 2;
+		j++;
+	}
+	else
+	{
+		new_str[0] = '1';
+		i = 1;
+	}
+	while (str[j] != '\0')
+		new_str[i++] = str[j++];
+	free(str);
+	*str_addr = new_str;
+	ft_putstr(*str_addr);
+}*/
+
+static char	*rounding_realloc(char *str, int sign)
+{
+	char	*new_str;
+	int		i;
+
+	i = ft_strlen(str);
+	if (!(new_str = (char*)ft_memalloc(sizeof(char) * (i + 1 + sign))))
+		return (NULL);
+	i = sign;
+	while (str[i - sign])
+	{
+		new_str[i] = str[i - sign];
+		i++;
+	}
+	if (sign)
+	{
+		new_str[0] = '-';
+		new_str[1] = '1';
+	}
+	else
+		new_str[0] = '1';
+	return (new_str);
+}
 
 static void		decimal_rounding(char **str_addr, int i)
 {
 	char	*str;
 
 	str = *str_addr;
-	while (str[i] == '9' && i >= 0)
+	while (str[i] == '9' && i > 0)
 	{
 		str[i] = '0';
 		i--;
@@ -25,12 +80,9 @@ static void		decimal_rounding(char **str_addr, int i)
 			i--;
 	}
 	if (str[i] == '-')
-	{
-		str[1] = '1';
-		str[0] = '-';
-	}
-	else if (i == 0)
-		str[0] = '1';
+		*str_addr = rounding_realloc(*str_addr, 1);
+	else if (i == -1)
+		*str_addr = rounding_realloc(*str_addr, 0);
 	else if (str[i] != '9')
 		str[i] += 1;
 }
@@ -59,7 +111,7 @@ static void		decimal(char **str_addr, double deci, int precis, int len)
 		if (str[i] != '9')
 			str[i] += 1;
 		else
-			decimal_rounding(&str, i);
+			decimal_rounding(str_addr, i);
 	}
 }
 
@@ -112,10 +164,10 @@ char			*ft_ftoa(long double n, int precis)
 	precis++;
 	whole = (long int)n;
 	len = count(whole);
-	if (!(str = (char*)ft_memalloc(sizeof(*str) * (len + precis + 2 + 1))))
+	if (!(str = (char*)ft_memalloc(sizeof(*str) * (len + precis + 2))))
 		return (NULL);
-	str[len + precis + 2] = '\0';
-	ft_itoa_float(&str, whole, len);
-	decimal(&str, FABS(n - (long double)whole), precis, len + 1);
-	return (str[0] == '\0' ? str + 1 : str);
+	str[len + precis + 1] = '\0';
+	ft_itoa_float(&str, whole, len - 1);
+	decimal(&str, FABS(n - (long double)whole), precis, len);
+	return (str);
 }
